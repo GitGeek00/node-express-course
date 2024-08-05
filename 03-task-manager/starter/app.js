120783a159dca5dd44737cbb4fd305922719ad40
Maher Algepah <maher@mahersoft.onmicrosoft.com>
2024-08-05 02:49:10 -0700

    week5 assignment

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,24 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const connectDB = require('./db/connect');
+const tasks = require('./routes/tasks');
+require('dotenv').config()
+
+app.use(express.json());
+
+app.use('/api/v1/tasks', tasks);
+
+
+const port = process.env.connectionPort || 5000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
